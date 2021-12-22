37beda1fcedaa1556233d88fa52a4cfa8cf5ecf3
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-12-22 13:43:35 -0500

    Used different colors and used the function i created and passed in the newButtonColor variable as a value

--- colorbutton/src/App.js
+++ colorbutton/src/App.js
@@ -7,10 +7,10 @@ export const replaceCamelWithSpaces = (colorName) => {
 
 function App() {
 
-  const [buttonColor, setButtonColor] = useState('red')
+  const [buttonColor, setButtonColor] = useState('MediumVioletRed')
   const [isButtonDisabled, setIsButtonDisabled] = useState(false)
 
-  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red'
+  const newButtonColor = buttonColor === 'MediumVioletRed' ? 'Midnight Blue' : 'MediumVioletRed'
 
   const handleClick = () => {
     setButtonColor(newButtonColor)
@@ -22,7 +22,7 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={handleClick} disabled={isButtonDisabled} style={{ backgroundColor: isButtonDisabled ? 'gray' : buttonColor }}>Change to {newButtonColor}</button>
+      <button onClick={handleClick} disabled={isButtonDisabled} style={{ backgroundColor: isButtonDisabled ? 'gray' : buttonColor }}>Change to {replaceCamelWithSpaces(newButtonColor)}</button>
       <input id='disable-button-checkbox' type='checkbox' onChange={handleCheckbox} />
       <label htmlFor='disable-button-checkbox'>Disable Button</label>
     </div>
