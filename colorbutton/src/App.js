95d562db9a575ebf5aeba9cd51951a2be1fd16c8
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-12-22 11:29:05 -0500

    Added a test to check that the checkbox is disabled on the first click and then enabled on the second click

--- colorbutton/src/App.js
+++ colorbutton/src/App.js
@@ -4,16 +4,22 @@ import { useState } from 'react'
 function App() {
 
   const [buttonColor, setButtonColor] = useState('red')
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false)
+
   const newButtonColor = buttonColor === 'red' ? 'blue' : 'red'
 
   const handleClick = () => {
     setButtonColor(newButtonColor)
   }
 
+  const handleCheckbox = (e) => {
+    setIsButtonDisabled(e.target.checked)
+  }
+
   return (
     <div className="App">
-      <button onClick={handleClick} style={{ backgroundColor: buttonColor }}>Change to {newButtonColor}</button>
-      <input type='checkbox' />
+      <button onClick={handleClick} disabled={isButtonDisabled} style={{ backgroundColor: buttonColor }}>Change to {newButtonColor}</button>
+      <input type='checkbox' onChange={handleCheckbox} />
     </div>
   );
 }
